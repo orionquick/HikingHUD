49a8b0897a727bcabd4e65830d809bb6c7334dbc
orionquick <orionquick@gmail.com>
2019-10-25 23:38:42 -0700

    added mapbox functionality

--- HikingHUD/app/src/main/java/com/example/hikinghud/MainActivity.java
+++ HikingHUD/app/src/main/java/com/example/hikinghud/MainActivity.java
@@ -4,11 +4,86 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import androidx.annotation.NonNull;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private int style = 0; // Satellite - 0, Mapbox = 1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Mapbox.getInstance(this, "pk.eyJ1Ijoib3Jpb25xdWljayIsImEiOiJjazI2ejl1bXYwYjc3M2hvNmgwY2Z4dTg0In0.nG7HWuR8E3KOl1qlZqLuzQ");
+        setContentView(R.layout.main_menu);
+        /*mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                if (style == 0) {
+                    mapboxMap.setStyle(Style.SATELLITE_STREETS, new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                        }
+                    });
+                }
+                else if (style == 1) {
+                    mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                        }
+                    });
+                }
+            }
+        });*/
+    }/*
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }*/
 }
